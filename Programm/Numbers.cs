dfbbdbee088f354021cc2deab6dcabfcce56c265
andreylar31415@gmail.com <andreylar31415@gmail.com>
2020-01-01 15:02:14 +0600

    ДОБАВИЛ: -WriteNum -WriteSum

---
+++ Programm/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm
+{
+    public class Numbers
+    {
+        
+
+        
+        public void WriteNum(int a,int b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+        public void WriteSum(int a,int b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+
+    }
+}
---
+++ Programm/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers num = new Numbers();
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            //num.WriteNum(a, b);
+            num.WriteSum(a, b);
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
